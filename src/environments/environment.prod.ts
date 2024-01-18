d080d6ed26808268ebb4681c2878d5aa98eca66a
lorenzo0895 <lorenzo0895@gmail.com>
2024-01-18 16:17:49 -0300

    Edit environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,13 @@
 export const environment = {
   production: true,
   authUrl: 'http://www.authapi.com',
+  firebase: {
+    apiKey: "AIzaSyB884qqLZ3hBTFJJdY636owzi-Zjhilfmw",
+    authDomain: "adoclic.firebaseapp.com",
+    projectId: "adoclic",
+    storageBucket: "adoclic.appspot.com",
+    messagingSenderId: "715056907918",
+    appId: "1:715056907918:web:e87f84c51b0d22fd322bdf",
+    measurementId: "G-RN6M2467TG"
+  }
 };
