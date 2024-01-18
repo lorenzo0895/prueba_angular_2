d031385ae6fef5bb04fd917e31a38eb1553e9651
lorenzo0895 <lorenzo0895@gmail.com>
2024-01-18 16:15:29 -0300

    Add firebase config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,17 +1,25 @@
 import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { fakeInterceptor } from '@shared/interceptors/fake.interceptor';
 import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
+import { environment } from 'src/environments/environment';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     { provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: { appearance: 'outline' } },
-    importProvidersFrom(BrowserAnimationsModule),
-    provideHttpClient(withInterceptors([fakeInterceptor]))
+    importProvidersFrom(
+      BrowserAnimationsModule,
+      provideFirebaseApp(() => initializeApp(environment.firebase)),
+      // provideAuth(() => getAuth()),
+      // provideFirestore(() => getFirestore()),
+      // provideStorage(() => getStorage())
+    ),
+    provideHttpClient(withInterceptors([fakeInterceptor])),
   ],
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,4 +2,13 @@
 export const environment = {
   production: false,
   authUrl: 'http://www.authapi.com',
+  firebase: {
+    apiKey: "AIzaSyB884qqLZ3hBTFJJdY636owzi-Zjhilfmw",
+    authDomain: "adoclic.firebaseapp.com",
+    projectId: "adoclic",
+    storageBucket: "adoclic.appspot.com",
+    messagingSenderId: "715056907918",
+    appId: "1:715056907918:web:e87f84c51b0d22fd322bdf",
+    measurementId: "G-RN6M2467TG"
+  }
 };
